008ee7196d8a0ebd90c076968975c8fa66a73426
evergreenempire83-sudo <evergreenempire83@gmail.com>
2025-12-11 08:35:12 +0100

    Create firebase.js

---
+++ frontend/src/services/firebase/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getFunctions } from 'firebase/functions';
+import { getStorage } from 'firebase/storage';
+import firebaseConfig from './config';
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const functions = getFunctions(app);
+export const storage = getStorage(app);
+
+// Export the app instance
+export default app;
